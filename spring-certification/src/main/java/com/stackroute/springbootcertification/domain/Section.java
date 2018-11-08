0b83ae8291f64d52c3834ac11546baf538144af8
akash.biswas <akash.biswas@cgi.com>
2018-11-08 09:52:40 +0530

    Certification

---
+++ spring-certification/src/main/java/com/stackroute/springbootcertification/domain/Section.java
@@ -0,0 +1,17 @@
+package com.stackroute.springbootcertification.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Section {
+    private String sectionId;
+    private String userId;
+    private String operationType;
+    private Chicklets chicklets;
+}
