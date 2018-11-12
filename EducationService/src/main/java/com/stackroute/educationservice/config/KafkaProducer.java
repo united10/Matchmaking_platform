8d9cc89ab78ef756303ae51927895c7c32c5cf47
uditvarshney <29udit@gmail.com>
2018-11-12 15:56:46 +0530

    Docker file added for education service

--- EducationService/src/main/java/com/stackroute/educationservice/config/KafkaProducer.java
+++ EducationService/src/main/java/com/stackroute/educationservice/config/KafkaProducer.java
@@ -29,11 +29,9 @@ public class KafkaProducer {
 
         Map<String, Object> config =new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getIpAddress());
-
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
+        config.put(JsonSerializer.ADD_TYPE_INFO_HEADERS,false);
         return new DefaultKafkaProducerFactory(config);
     }
 
