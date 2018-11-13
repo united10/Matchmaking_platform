949d5eb673f2605a2933ed6fcc9caee6b1d130e3
akash.biswas <akash.biswas@cgi.com>
2018-11-13 11:45:37 +0530

    Updated

--- CertificationService/src/main/java/com/stackroute/listener/KafkaListening.java
+++ CertificationService/src/main/java/com/stackroute/listener/KafkaListening.java
@@ -17,6 +17,7 @@ public class KafkaListening {
     @KafkaListener(topics = "${kafka.listeningTopic}" ,groupId = "${kafka.groupId}",
             containerFactory="${kafka.containerFactory}")
     public void consumeJson(@Payload Section section) {
+        System.out.print("Consumed Message "+ section);
         Relationships[] relationships=new Relationships[1];
         Relationships relationships1=new Relationships("working","is-a");
         relationships[0]=relationships1;
