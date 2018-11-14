9acaf4ad43a273d7b848b6389dd077fc4582fb28
palashmoon <palashmoon8@gmail.com>
2018-11-14 15:26:27 +0530

    location ui integrated

--- angular/src/app/cards/detailsdialog/detailsdialog.component.ts
+++ angular/src/app/cards/detailsdialog/detailsdialog.component.ts
@@ -3,12 +3,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { EducationdialogComponent } from '../educationdialog/educationdialog.component';
 import { ExperienceComponent } from '../experience/experience.component';
-<<<<<<< HEAD
 import { SkillComponent } from '../skill/skill.component';
-//import {SkillComponent}
-=======
-import { CertificatedialogComponent } from '../certificatedialog/certificatedialog.component';
->>>>>>> 9c196e8daf33e7675773fa7bf723ce0f6a0673bd
+import { LocationdialogComponent } from '../locationdialog/locationdialog.component';
 
 @Component({
   selector: 'app-detailsdialog',
@@ -45,8 +41,6 @@ export class DetailsdialogComponent implements OnInit {
     dialogConfig.width = '40%';
     this.dialog.open(ExperienceComponent, dialogConfig);
   }
-<<<<<<< HEAD
-
   skillClick() {
     this.dialogRef.close();
     const dialogConfig = new MatDialogConfig();
@@ -54,20 +48,21 @@ export class DetailsdialogComponent implements OnInit {
     dialogConfig.autoFocus = true;
     dialogConfig.width = '40%';
     this.dialog.open(SkillComponent, dialogConfig);
-    
   }
-=======
->>>>>>> 9c196e8daf33e7675773fa7bf723ce0f6a0673bd
   certificateClick() {
     this.dialogRef.close();
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = false;
     dialogConfig.autoFocus = true;
-<<<<<<< HEAD
-    dialogConfig.width='40%';
-=======
     dialogConfig.width = '40%';
->>>>>>> 9c196e8daf33e7675773fa7bf723ce0f6a0673bd
     this.dialog.open(CertificatedialogComponent, dialogConfig);
+  }
+  locationClick() {
+    this.dialogRef.close();
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '40%';
+    this.dialog.open(LocationdialogComponent, dialogConfig);
   }
 }
