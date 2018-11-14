e3c69d164192e96de6ebc15a85f1466d38987dc4
suhrid.barthakur <suhrid.barthakur@cgi.com>
2018-11-14 15:46:31 +0530

    Frontend integration

--- angular/src/app/cards/cards.module.ts
+++ angular/src/app/cards/cards.module.ts
@@ -12,10 +12,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ExperienceComponent } from './experience/experience.component';
-<<<<<<< HEAD
 import { SkillComponent } from './skill/skill.component';
-=======
->>>>>>> 9c196e8daf33e7675773fa7bf723ce0f6a0673bd
 import { CertificatedialogComponent } from './certificatedialog/certificatedialog.component';
 
 @NgModule({
@@ -31,13 +28,8 @@ import { CertificatedialogComponent } from './certificatedialog/certificatedialo
     MatDatepickerModule,
     MatNativeDateModule
   ],
-<<<<<<< HEAD
-  declarations: [CardComponent, DetailsdialogComponent, EducationdialogComponent, ExperienceComponent, SkillComponent,CertificatedialogComponent],
-  entryComponents: [DetailsdialogComponent, EducationdialogComponent,SkillComponent,CertificatedialogComponent],
-=======
-  declarations: [CardComponent, DetailsdialogComponent, EducationdialogComponent, ExperienceComponent, CertificatedialogComponent],
-  entryComponents: [DetailsdialogComponent, EducationdialogComponent, CertificatedialogComponent],
->>>>>>> 9c196e8daf33e7675773fa7bf723ce0f6a0673bd
+  declarations: [CardComponent, DetailsdialogComponent, EducationdialogComponent, ExperienceComponent,SkillComponent, CertificatedialogComponent],
+  entryComponents: [DetailsdialogComponent, EducationdialogComponent, CertificatedialogComponent,SkillComponent],
   exports: [CardComponent]
 })
 export class CardsModule { }
