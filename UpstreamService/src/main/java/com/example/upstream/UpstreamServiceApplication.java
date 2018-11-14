adecafcde7f43be43347687a933993dc05647685
kevin.shah <kevin.shah@cgi.com>
2018-11-15 00:49:04 +0530

    Added Eureka Client

--- UpstreamService/src/main/java/com/example/upstream/UpstreamServiceApplication.java
+++ UpstreamService/src/main/java/com/example/upstream/UpstreamServiceApplication.java
@@ -4,14 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UpstreamServiceApplication {
 
-	static Logger logger= LoggerFactory.getLogger(UpstreamServiceApplication.class);
 
 	public static void main(String[] args) {
-	    logger.info("Hi, I am in main");
 		SpringApplication.run(UpstreamServiceApplication.class, args);
 	}
 }
