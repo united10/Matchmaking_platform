c43dd95fe675d589979ddc735d41a3432f930fc1
hukma.ram <hukma.ram@cgi.com>
2018-11-15 18:02:09 +0530

    Login Front End push

---
+++ angular/src/app/login/login/login.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+    HttpClientModule
+  ],
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
+})
+export class LoginModule { }
