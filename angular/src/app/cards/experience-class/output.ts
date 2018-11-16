1b2f4fec1b2caff695b32f40f443ce0a36e04e75
udit.kumar <udit.kumar@cgi.com>
2018-11-16 15:48:40 +0530

    Update output.ts

--- angular/src/app/cards/experience-class/output.ts
+++ angular/src/app/cards/experience-class/output.ts
@@ -13,14 +13,4 @@ export class Output {
         this.terminalProperties = terminalProperties;
         this.relation = relation;
     }
-<<<<<<< HEAD
-
-    // operationType: string;
-    // sourceNode: string;
-    // sourceProperty: string;
-    // terminalNode: string;
-    // terminalProperties: string;
-    // relationShip: Relationship;
-=======
->>>>>>> 9e7726043531d9d4e524b8a080637846f68a9cb7
 }
