0b05e0b86bd4e51caacc2e4f9dbcd2945905a7e4
udit.kumar <udit.kumar@cgi.com>
2018-11-16 15:19:56 +0530

    Update cards.module.ts

--- angular/src/app/cards/cards.module.ts
+++ angular/src/app/cards/cards.module.ts
@@ -15,10 +15,7 @@ import { ExperienceComponent } from './experience/experience.component';
 import { SkillComponent } from './skill/skill.component';
 import { CertificatedialogComponent } from './certificatedialog/certificatedialog.component';
 import { LocationdialogComponent } from './locationdialog/locationdialog.component';
-<<<<<<< HEAD
-=======
 import { ProjectdialogComponent } from './projectdialog/projectdialog.component';
->>>>>>> 9e7726043531d9d4e524b8a080637846f68a9cb7
 
 @NgModule({
   imports: [
@@ -33,49 +30,24 @@ import { ProjectdialogComponent } from './projectdialog/projectdialog.component'
     MatDatepickerModule,
     MatNativeDateModule
   ],
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  declarations: [CardComponent, DetailsdialogComponent, EducationdialogComponent, ExperienceComponent],
-  entryComponents: [DetailsdialogComponent, EducationdialogComponent, ExperienceComponent],
-=======
-<<<<<<< HEAD
-  declarations: [CardComponent, DetailsdialogComponent, EducationdialogComponent, ExperienceComponent, SkillComponent,CertificatedialogComponent],
-  entryComponents: [DetailsdialogComponent, EducationdialogComponent,SkillComponent,CertificatedialogComponent],
-=======
-  declarations: [CardComponent, DetailsdialogComponent, EducationdialogComponent, ExperienceComponent, CertificatedialogComponent],
-  entryComponents: [DetailsdialogComponent, EducationdialogComponent, CertificatedialogComponent],
->>>>>>> 9c196e8daf33e7675773fa7bf723ce0f6a0673bd
->>>>>>> 852ac48a5695665c9008b18b404a4035b0801772
-=======
-=======
 
->>>>>>> 9e7726043531d9d4e524b8a080637846f68a9cb7
   declarations: [CardComponent,
                   DetailsdialogComponent,
                   EducationdialogComponent,
-                  ExperienceComponent,
                   SkillComponent,
                   CertificatedialogComponent,
-<<<<<<< HEAD
-                  LocationdialogComponent],
-=======
                   LocationdialogComponent,
                   ExperienceComponent,
                   ProjectdialogComponent],
->>>>>>> 9e7726043531d9d4e524b8a080637846f68a9cb7
+
   entryComponents: [DetailsdialogComponent,
                     EducationdialogComponent,
                     SkillComponent,
                     CertificatedialogComponent,
-<<<<<<< HEAD
-                    LocationdialogComponent],
->>>>>>> fee8a28e90d5620b2d6014cef17cf39656d3b12a
-=======
                     ExperienceComponent,
                     LocationdialogComponent,
                     ProjectdialogComponent],
->>>>>>> 9e7726043531d9d4e524b8a080637846f68a9cb7
+
   exports: [CardComponent]
 })
 export class CardsModule { }
