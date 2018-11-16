3ac5f0ffed1254912a90db47abfd45a52bb891d3
udit.kumar <udit.kumar@cgi.com>
2018-11-16 15:46:46 +0530

    Update chicklets.ts

--- angular/src/app/cards/experience-class/chicklets.ts
+++ angular/src/app/cards/experience-class/chicklets.ts
@@ -4,9 +4,4 @@ export class Chicklets {
     constructor(private experienceDetails: ExperienceDetails) {
         this.experienceDetails = experienceDetails;
     }
-<<<<<<< HEAD
-
-    // experienceDetails: ExperienceDetails;
-=======
->>>>>>> 9e7726043531d9d4e524b8a080637846f68a9cb7
 }
