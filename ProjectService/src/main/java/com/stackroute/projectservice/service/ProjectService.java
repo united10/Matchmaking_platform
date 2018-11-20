696a99f4dc16fb9fb71735cf1e85f5229cff5c96
saivishal.muda <saivishal.muda@cgi.com>
2018-11-20 11:50:40 +0530

    added comments

--- ProjectService/src/main/java/com/stackroute/projectservice/service/ProjectService.java
+++ ProjectService/src/main/java/com/stackroute/projectservice/service/ProjectService.java
@@ -3,6 +3,8 @@ package com.stackroute.projectservice.service;
 import com.stackroute.projectservice.domain.CommonOutput;
 import com.stackroute.projectservice.domain.Section;
 
+/*It is an interface for the certificate service
+for loosely coupling and changes for future*/
 public interface ProjectService {
     CommonOutput processProjectDetails(Section section);
 }
