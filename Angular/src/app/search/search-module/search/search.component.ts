0efa61ca58055cb2602e05165ac8f6823f652d9d
saivishal.muda <saivishal.muda@cgi.com>
2018-11-23 13:05:31 +0530

    changes to search component

--- Angular/src/app/search/search-module/search/search.component.ts
+++ Angular/src/app/search/search-module/search/search.component.ts
@@ -20,47 +20,6 @@ export class SearchComponent implements OnInit {
     if (this.tokenstorageservice.getToken()) {
         this.isLoggedIn = true;
       }
-    const s1 = this._renderer2.createElement('script');
-        s1.type = `application/ld+json`;
-        s1.text = `
-            {
-                "@context": "/p5.js"
-            }
-        `;
-        const s2 = this._renderer2.createElement('script');
-        s2.type = `application/ld+json`;
-        s2.text = `
-            {
-                "@context": "./p5/addons/p5.dom.js"
-            }
-        `;
-        const s3 = this._renderer2.createElement('script');
-        s3.type = `application/ld+json`;
-        s3.text = `
-            {
-                "@context": "./p5/addons/p5.sound.js"
-            }
-        `;
-        const s4 = this._renderer2.createElement('script');
-        s4.type = `application/ld+json`;
-        s4.text = `
-            {
-                "@context": "./p5/addons/p5.speech.js"
-            }
-        `;
-        const s5 = this._renderer2.createElement('script');
-        s5.type = `application/ld+json`;
-        s5.text = `
-            {
-                "@context": "./p5/sketch.js"
-            }
-        `;
-
-        this._renderer2.appendChild(this._document.body, s1);
-        this._renderer2.appendChild(this._document.body, s2);
-        this._renderer2.appendChild(this._document.body, s3);
-        this._renderer2.appendChild(this._document.body, s4);
-        this._renderer2.appendChild(this._document.body, s5);
   }
   logout() {
     this.tokenstorageservice.signOut();
