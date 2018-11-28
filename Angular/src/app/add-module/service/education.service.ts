ef7c7bd7c953a796bb21352772555c447e244690
hukma.ram <hukma.ram@cgi.com>
2018-11-28 12:25:40 +0530

    Autocomplete added in one field

--- Angular/src/app/add-module/service/education.service.ts
+++ Angular/src/app/add-module/service/education.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { EducationSection } from '../education-dialog/domain/educationsection';
 import { Output } from '../outputclass/output';
+import { IQualificationResponse, Qualificationn } from '../education-dialog/domain/qualificationn';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -36,5 +37,16 @@ export class EducationService {
     }
     // return an observable with a user-facing error message
     return throwError('Something bad happened; please try again later.');
+  }
+  search(filter: {name: string} = {name: ''}, page = 1): Observable<IQualificationResponse> {
+    console.log('inside service ' + filter.name);
+    return this.httpClient.get<IQualificationResponse>('http://172.23.239.135:8081/api/v1/redisEducation/' + filter.name)
+    .pipe(
+      tap((response: IQualificationResponse) => {
+        response.educations = response.educations
+          .map(qualification => new Qualificationn(qualification.name, qualification.id));
+        return response;
+      })
+      );
   }
 }
