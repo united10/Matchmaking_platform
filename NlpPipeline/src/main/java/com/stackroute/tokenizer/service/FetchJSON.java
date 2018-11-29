9fff8e4b21767e33e018c1bb9d9616834fd49bac
uditvarshney <29udit@gmail.com>
2018-11-29 22:15:02 +0530

    logs added for fetching json in nlp pipeline

--- NlpPipeline/src/main/java/com/stackroute/tokenizer/service/FetchJSON.java
+++ NlpPipeline/src/main/java/com/stackroute/tokenizer/service/FetchJSON.java
@@ -1,5 +1,7 @@
 package com.stackroute.tokenizer.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,9 +12,12 @@ public class FetchJSON {
 
 private final static  String url="http://13.233.180.226:8008/api/v1/output";     // This hardcode is for testing
 
+    Logger logger = LoggerFactory.getLogger(FetchJSON.class);
     public String  getAllData(){
+        logger.info("----------------------------Fetching JSON --------------------------------");
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
+        logger.info("Fetched Json: {}",result);
         return result;
     }
 }
