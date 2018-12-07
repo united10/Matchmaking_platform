075bceb1cd4c84cfdd3a052ddcf949dfc71d6b27
suhrid.barthakur <suhrid.barthakur@cgi.com>
2018-12-07 12:22:36 +0530

    Query Engine domain

---
+++ QueryEngine/src/main/java/com/stackroute/queryengine/domain/employee/BasicInfo.java
@@ -0,0 +1,19 @@
+package com.stackroute.queryengine.domain.employee;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasicInfo {
+    private String dob;
+    private String githubUrl;
+    private String linkedinUrl;
+    private String gender;
+    private String contactNo;
+}
---
+++ WebsocketService/src/main/java/com/example/WebsocketService/domain/BasicInfo.java
@@ -0,0 +1,19 @@
+package com.example.WebsocketService.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasicInfo {
+    private String dob;
+    private String githubUrl;
+    private String linkedinUrl;
+    private String gender;
+    private String contactNo;
+}
